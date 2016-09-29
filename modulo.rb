e5f424a5cf76f3a532a75c82cb28e64630052ec9
Damon Clark <dclark312@gmail.com>
2016-09-29 10:07:43 -0400

    second exercise

--- modulo.rb
+++ modulo.rb
@@ -1,3 +1,12 @@
+=begin
+Use the modulo operator, division, or a combination 
+of both to take a 4 digit number and find the digit in the:
+1) thousands place
+2) hundreds place
+3) tens place
+4) ones place
+=end
+
 puts 1576 / 1000
 puts 1576 % 1000 / 100 
 puts 1576 % 100 / 10
