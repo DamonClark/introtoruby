dc56d3e8ad2d3a0ea5660bd67929453c21617833
Damon Clark <dclark312@gmail.com>
2016-09-29 11:53:31 -0400

    Adding exercises for Variables section

--- thebasics/array.rb
+++
@@ -1,9 +0,0 @@
-=begin
- Use the dates from the previous example and store 
- them in an array. Then make your program output 
- the same thing as exercise 3. 
-=end
-
-dates = [1975, 2004, 2013, 2001, 1981]
-
-puts dates(0)
