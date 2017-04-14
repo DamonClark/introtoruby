c2739eb7c042267d5e3cd0d2a0c975860065f4be
Damon Clark <dclark312@gmail.com>
2017-04-14 10:32:33 -0400

    Exercise5

--- loopsiterators/example3.rb
+++ loopsiterators/example3.rb
@@ -3,3 +3,9 @@ Use the each_with_index method to iterate through an array of
 your creation that prints each index and value of the array.
 =end
 
+musicians = ["Bob Dylan", "George Martin", 
+         "Brian Eno", "George Harrison", "Ringo"]
+
+musicians.each_with_index do | musicians, index |
+  puts "#{index + 1}. #{musicians}"
+end
