135abb614772915c56f62fc6fba4bfb35161bb64
Damon Clark <dclark312@gmail.com>
2017-04-14 10:35:42 -0400

    Arrays

---
+++ arrays/solution.rb
@@ -0,0 +1,6 @@
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+puts words.each  { |x| puts words }
+
