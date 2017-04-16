683f537d0cfe16672737d3967a70a7447b863428
Damon Clark <dclark312@gmail.com>
2017-04-16 14:22:32 -0400

    More Exercises

---
+++ loopsiterators/example2.rb
@@ -0,0 +1,5 @@
+x = ""
+while x != "STOP"
+  puts "Please type any word into the console window"
+  x = gets.chomp.to_s
+end
---
+++ loopsiterators/example4.rb
@@ -0,0 +1,11 @@
+def countdown(x)
+  if x <= 0
+    puts x
+  else 
+    puts x
+    countdown(x - 1)
+  end 
+end
+
+countdown(9)
+
